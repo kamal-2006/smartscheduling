009401849b5cfeb9d1d7683bf69f7e52f870fec7
kamal-2006 <kamaleshkamales006@gmail.com>
2025-07-22 20:00:00 +0530

    first commit

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../pages/login/index";
+import Doctor from "../pages/main/doctor/doctor";
+import Patient from "../pages/main/patient/patient";
+
+export const AppRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/doctor" element={<Doctor />} />
+        <Route path="/patient" element={<Patient />} />
+
+        <Route path="*" element={<div> Not found</div>} />
+      </Routes>
+    </>
+  );
+};
